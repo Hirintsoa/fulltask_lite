6395a07262bdf72e41aa9f7d54cda6b8525da5ad
Hirintsoa <hirintsoa23@protonmail.com>
2025-06-16 13:51:29 +0300

    chore: add task infos in task list show page

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -2,4 +2,12 @@ module TasksHelper
   def task_priority_options
     Task.priorities.keys.map { |key| [key.titleize, key] }
   end
+
+  def format_priority(priority)
+    case priority
+    when "low" then content_tag(:span, "Low", class: "badge badge-secondary")
+    when "medium" then content_tag(:span, "Medium", class: "badge badge-warning")
+    when "high" then content_tag(:span, "High", class: "badge badge-danger")
+    end
+  end
 end
