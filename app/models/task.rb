f8ef14e7893c2a2f4fbddbc2cc86faaf6e282eaf
Hirintsoa <hirintsoa23@protonmail.com>
2025-06-16 08:32:30 +0300

    build: configure models attributes and scopes

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,17 @@
 class Task < ApplicationRecord
   belongs_to :task_list
+
+  attribute :completed, :boolean, default: false
+  enum :priority, [ :low, :medium, :high ], default: :low, validate: true
+  validates :title, presence: true, uniqueness: { scope: :task_list_id }
+
+  before_validation :normalize_title
+
+  scope :incomplete, -> { where(completed: false) }
+
+  private
+
+  def normalize_title
+    self.title = title.to_s.strip.titleize if title.present?
+  end
 end
--- app/models/task_list.rb
+++ app/models/task_list.rb
@@ -1,2 +1,13 @@
 class TaskList < ApplicationRecord
+  has_many :tasks, dependent: :destroy
+  accepts_nested_attributes_for :tasks
+
+  validates :title, presence: true, uniqueness: true
+  before_validation :normalize_title
+
+  private
+
+  def normalize_title
+    self.title = title.to_s.strip.titleize if title.present?
+  end
 end
